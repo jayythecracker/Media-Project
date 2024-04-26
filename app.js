40a458cf6d5838076aa143b8d9a78120e94f0e30
Jayy <86412100+jayythecracker@users.noreply.github.com>
2024-04-26 23:43:43 +0630

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,34 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload')
-mongoose.set('strictQuery', false);
+//H7phtdVEZTswlTbd
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jayythecracker:H7phtdVEZTswlTbd@jayy.ryk5chg.mongodb.net/?retryWrites=true&w=majority&appName=jayy";
 
-mongoose.connect(`mongodb://54.254.237.32:27017/${process.env.DB_NAME}`);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.use(express.json());
 app.use(fileUpload());
